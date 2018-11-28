19593dc1979596e59285813e4cd739d0af3ab27d
Gracey Wilson <gracey.wilson@students.olin.edu>
2018-11-28 17:37:17 -0500

    very prelim outline

---
+++ generate_key.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+'''Pick 2 random primes (have size as a changeable input?)'''
+
+'''Find least common multiple'''
+
+'''Generate public key e: Choose a positive integer e that’s < LCM and coprime with LCM'''
+
+'''Generate private key d: Find the modular multiplicative inverse d of (e mod LCM) s.t. the remainder after dividing d * e by LCM is 1'''
